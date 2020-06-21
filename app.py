894ce4bed2e0496df48497795329771690fa37b8
Shir Biton <shirbi10@gmail.com>
2020-06-21 12:05:45 +0300

    Added error handling

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "./files"
 
 ### VIRUSTOTAL CONSTS ###
-VIRUSTOTAL_BASE_URL = "https://www.virustotal.com/api/v3"
+VIRUSTOTAL_BASE_URL = "https://www.virustotal.com/api/v"
 VIRUSTOTAL_API_KEY = "7b796cd4b1043a7dabba77bd730374a9b3a1f31425b0c5fc54eaf5d89bc22fbf"
 
 def init_loggers():
@@ -38,11 +38,14 @@ def upload_file():
     if "file" not in request.files:
             return make_response(jsonify({"error": "No file received"}), 400)
 
-    input_file = request.files["file"]
-    file_name = secure_filename(input_file.filename)
-    input_file.save(os.path.join(app.config["UPLOAD_FOLDER"], file_name))
+    try:
+        input_file = request.files["file"]
+        file_name = secure_filename(input_file.filename)
+        input_file.save(os.path.join(app.config["UPLOAD_FOLDER"], file_name))
 
-    return make_response(scan_file(file_name), 200)
+        return make_response(scan_file(file_name))
+    except Exception as e:
+        return make_response(jsonify({"error": "Bad Request"}), 400)
 
 def scan_file(file_name):
     """
@@ -51,14 +54,21 @@ def scan_file(file_name):
     :return: Virustotal json format analysis
     """
     headers = {"x-apikey": VIRUSTOTAL_API_KEY}
-    files = {"file": open(os.path.join(app.config["UPLOAD_FOLDER"], file_name), "rb")}
 
-    res = requests.post(VIRUSTOTAL_BASE_URL + "/files", headers=headers, files=files)
-    res_id = res.json()["data"]["id"]
-    
-    analysis_info = requests.get(VIRUSTOTAL_BASE_URL + f"/analyses/{res_id}", headers=headers)
+    try:
+        files = {"file": open(os.path.join(app.config["UPLOAD_FOLDER"], file_name), "rb")}
+    except:
+        return {"error": "Invalid file path"}
 
-    return analysis_info.json()
+    try:
+        res = requests.post(VIRUSTOTAL_BASE_URL + "/files", headers=headers, files=files)
+        res_id = res.json()["data"]["id"]
+        
+        analysis_info = requests.get(VIRUSTOTAL_BASE_URL + f"/analyses/{res_id}", headers=headers)
+
+        return analysis_info.json()
+    except:
+        return {"error": "Unable to connect to virustotal service"}
 
 
 if __name__ == "__main__":
